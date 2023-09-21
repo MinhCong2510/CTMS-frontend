e44ab6b6ea0668380a97307516b74e18b53e4b2b
JLee00101 <leejc01@hotmail.com>
2023-09-22 01:49:59 +1000

    refresh

---
+++ src/components/staff/staff.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames';
+import styles from './staff.module.scss';
+import { Sidebar } from '../sidebar/sidebar';
+
+export interface StaffProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+
+export const Staff = ({ className }: StaffProps) => {
+    return <div className={classNames(styles.root, className)}>
+        <Sidebar />
+        <div>
+            <div>Staffs
+                <div className={styles.staffProfile}>
+                    <img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" alt=""
+                        style={{
+                            width: 100,
+                            height: 100,
+                        }}
+                    />
+                    <div className={styles.staffField}>
+                        <label>Name:</label>
+                        <label>Position:</label>
+                        <label>Involved Projects:</label>
+                        <label>Certification: </label>
+                    </div>
+                </div>
+                <div className={styles.staffProfile}>
+                    <img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" alt=""
+                        style={{
+                            width: 100,
+                            height: 100,
+                        }}
+                    />
+                    <div className={styles.staffField}>
+                        <label>Name:</label>
+                        <label>Position:</label>
+                        <label>Involved Projects:</label>
+                        <label>Certification: </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
+};
