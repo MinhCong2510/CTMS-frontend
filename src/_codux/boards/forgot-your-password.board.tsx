53a45090839b724baddb1b51bc708dcb891810ae
JLee00101 <141626254+JLee00101@users.noreply.github.com>
2023-09-21 22:14:03 +1000

    Delete src directory

--- src/_codux/boards/forgot-your-password.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-
-export default createBoard({
-    name: 'Forgot your password',
-    Board: () => <div className="forgotPBackground"></div>,
-    isSnippet: true,
-});
